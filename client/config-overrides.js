8fca52cbaa0ca2aec974d7e7ae122a1998174954
Shvembldr <shvembldr@yandex.ru>
2018-06-29 20:24:07 +0300

    add readme

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,10 +2,7 @@ const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', style: true }],
-    config
-  );
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
   config = rewireLess.withLoaderOptions({
     modifyVars: { '@primary-color': '#1DA57A' },
     javascriptEnabled: true
