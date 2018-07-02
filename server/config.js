6beeaed1e257049bc3211e1a6918f5247f865013
Shvembldr <shvembldr@yandex.ru>
2018-07-02 12:43:09 +0300

    heroku dbconfig fix

--- server/config.js
+++ server/config.js
@@ -7,23 +7,36 @@ const operatorsAliases = { $like: Op.like, $any: Op.any, $between: Op.between };
 
 dotenv.config();
 
-export default {
-  ENVIRONMENT: process.env.NODE_ENV,
-  SECRET: process.env.SECRET,
-  db:
-    process.env.NODE_ENV === 'test'
-      ? {
+const dbConfig = () => {
+  switch (process.env.NODE_ENV) {
+    case 'test':
+      return {
         dialect: 'postgres',
         url: process.env.TEST_DATABASE_URL,
         database: 'database_test',
         operatorsAliases,
         logging: false,
-      }
-      : {
+      };
+    case 'production':
+      return {
+        dialect: 'postgres',
+        url: process.env.DATABASE_URL,
+        operatorsAliases: false,
+        logging: false,
+      };
+    default:
+      return {
         dialect: 'postgres',
         url: process.env.DATABASE_URL,
         database: 'lissa',
         operatorsAliases,
         logging: false,
-      },
+      };
+  }
+};
+
+export default {
+  ENVIRONMENT: process.env.NODE_ENV,
+  SECRET: process.env.SECRET,
+  db: dbConfig(),
 };
